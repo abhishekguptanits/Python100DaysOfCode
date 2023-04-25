863ad372860ed1aef8d8e6eb743593adf772d36b
Abhishek <abhishekguptanits@gmail.com>
2023-04-25 23:37:06 +0530

    added exception handling for the user input in Nato Alphabet Project

--- day-26-List-Comprehension-AND-NATO-Alphabet/day-26-005-NATO-Alphabet-Project/main.py
+++ day-26-List-Comprehension-AND-NATO-Alphabet/day-26-005-NATO-Alphabet-Project/main.py
@@ -1,17 +1,30 @@
 import pandas
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in df.iterrows()}
-print(phonetic_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input('Enter any word: ')
 
-phonetic_list = [phonetic_dict[letter.upper()] for letter in word]
-print(phonetic_list)
+# flag = True
+#
+# while flag:
+#     word = input('Enter any word: ')
+#     try:
+#         phonetic_list = [phonetic_dict[letter.upper()] for letter in word]
+#     except KeyError:
+#         print("Only Alphabet letters are allowed, Please try again!")
+#     else:
+#         print(phonetic_list)
+#         flag = False
 
+
+def generate_phonetic():
+    word = input('Enter any word: ')
+    try:
+        phonetic_list = [phonetic_dict[letter.upper()] for letter in word]
+    except KeyError:
+        print("Only Alphabet letters are allowed, Please try again!")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
+
+generate_phonetic()
